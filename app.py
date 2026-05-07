f5023c393c1ca872ec7b30dd7dcba883d93e593c
Leonardo Almeida <61922601+leonardoalmeida095@users.noreply.github.com>
2026-05-07 19:45:56 +0000

    commit inicial

---
+++ app.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    # Lê a variável de ambiente injetada pelo Cloud Run. O padrão é 'dev-local'
+    version = os.environ.get("APP_VERSION", "dev-local")
+    
+    html = f"""
+    <html>
+        <body style="font-family: Arial, sans-serif; text-align: center; margin-top: 50px;">
+            <h1>☁️ Teste de Tráfego Cloud Run</h1>
+            <h2>Versão atual da aplicação: <span style="color: blue;">{version}</span></h2>
+        </body>
+    </html>
+    """
+    return html
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
