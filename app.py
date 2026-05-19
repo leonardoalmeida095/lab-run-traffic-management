cdb980320f91a8dde5d2cc70702be7a121dc8a17
leonardoalmeida095 <leonardo.souza0995@gmail.com>
2026-05-19 10:00:47 -0300

    teste

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def index():
     html = f"""
     <html>
         <body style="font-family: Arial, sans-serif; text-align: center; margin-top: 50px; background-color: #f0f8ff;">
-            <h1>🚀 Teste de Tráfego Cloud Run (V6.0.4)</h1>
+            <h1>🚀 Teste de Tráfego Cloud Run (V6.3.0)</h1>
             <h2>Versão do Preview: <span style="color: green;">{version}</span></h2>
         </body>
     </html>
